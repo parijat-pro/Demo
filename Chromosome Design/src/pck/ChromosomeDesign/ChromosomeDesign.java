96322816cc50191fc817cc11f83b66dd320dab10
Sudipta <kol.cgr@gmail.com>
2021-04-18 02:05:55 +0530

    3rd Update

--- Chromosome Design/src/pck/ChromosomeDesign/ChromosomeDesign.java
+++ Chromosome Design/src/pck/ChromosomeDesign/ChromosomeDesign.java
@@ -1,22 +1,80 @@
 package pck.ChromosomeDesign;
 
 public class ChromosomeDesign {
-    Chromosome[] chromosomes=new Chromosome[10];
+    public static Chromosome[] chromosomes=new Chromosome[10];
     int row,col;
-    TimeSlotDetails slotDetails;
+    private static int TOTAL_SUBJECTS=7;
+    private TimeSlotDetails slotDetails;
+    private Subject[] subjects=new Subject[TOTAL_SUBJECTS];
 
-    /*
+    public ChromosomeDesign() {
+        subjects[0]=new Subject("Programming for problem solving","MCAC101");
+        subjects[1]=new Subject("Discreet Structure","MCAC103");
+        subjects[2]=new Subject("Soft skills","MCAS101");
+        subjects[3]=new Subject("Computer Networks","MCAC102");
+        subjects[4]=new Subject("Introduction to Artificial Intelligence"," MCAD101D");
+        subjects[5]=new Subject("Computer Networks","MCAC192");
+        subjects[6]=new Subject("Programming for problem solving","MCAC191");
 
-    for(int i=0;i<10;i++){
-        for(int j=0;j<7;j++){
-            //rand row & col;
+    }
 
-            chromosomes[i].setClass(row,col,new ClassDetails(false,...));
+    private void setPopulation(){
+        for(int chromosomeNumber=0;chromosomeNumber<10;chromosomeNumber++){
+            chromosomes[chromosomeNumber]=new Chromosome();
+            for(int classNumber=0;classNumber<7;classNumber++){
+                while(true) {
+                    row = (int) (Math.random() * (4 + 1));//(limit+1)
+                    col = (int) (Math.random() * (5 + 1));
+                    if(!chromosomes[chromosomeNumber].isAllotted(row,col)){
+                        int subjectNumber=(int) (Math.random() * (6 + 1));
+                        chromosomes[chromosomeNumber].setClass(row,col,new ClassDetails(subjectNumber>=5,
+                                new TimeSlotDetails(getDayFullName(row), col+1),subjects[subjectNumber].getRespectiveTeacher(),subjects[subjectNumber]));
+                        break;
+                    }
+                }
+            }
+            chromosomes[chromosomeNumber].getFitness();
         }
-        chromosomes[i].getFitness();
-    }*/
+    }
+
+    private String getDayFullName(int row) {
+        switch (row){
+            case 0:
+                return "Monday";
+            case 1:
+                return "Tuesday";
+            case 2:
+                return "Wednesday";
+            case 3:
+                return "Thursday";
+            case 4:
+                return "Friday";
+            default:
+                return "";
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("hi");
+        ChromosomeDesign population=new ChromosomeDesign();
+        population.setPopulation();
+        population.displayPopulation();
+    }
+
+    private void displayPopulation() {
+        for(int chromosomeCounter=0;chromosomeCounter<10;chromosomeCounter++){
+            System.out.println("Chromosome "+chromosomeCounter+":");
+            for (int row=0;row<5;row++){
+                for(int col=0;col<6;col++){
+                    ClassDetails details=chromosomes[chromosomeCounter].getClassDetails(row,col);
+                    if(details!=null)
+                        System.out.print("["+details.isPracticalClass()+","+details.getSubject().subjectCode+","+details.getTeacher().getTeacherAbbreviation()+"]   ");
+                    else
+                        System.out.print("\t--\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
     }
 
 }
--- Chromosome Design/src/pck/ChromosomeDesign/Subject.java
+++ Chromosome Design/src/pck/ChromosomeDesign/Subject.java
@@ -18,4 +18,23 @@ public class Subject{
     public String getSubjectCode() {
         return subjectCode;
     }
+
+    public Teacher getRespectiveTeacher() {
+        
+        switch (this.subjectCode){
+            case "MCAC101":
+            case "MCAD101D":
+            case "MCAC191":
+                return (new Teacher("Anindita Das Bhattacharya","ADB"));
+            case "MCAC103":
+            case "MCAC102":
+            case "MCAC192":
+                return (new Teacher("Gourav Mondal","GM"));
+            case "MCAS101":
+                return (new Teacher("Sushanta Kayal","SK"));
+            default:
+                return (new Teacher("",""));
+
+        }
+    }
 }
